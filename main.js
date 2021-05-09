9e5551d75c826063a7e9b289a2f9974948c10d69
SrideviK <sridevibasavaraju@gmail.com>
2021-05-09 12:59:47 -0700

    added modified files

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ let sportsArray = [];
 let selectedType = "";
 
 // define a constructor to create sport objects
-let SportObject = function (pTeamName, pPlayerName,pSport) {
+let SportObject = function (pTeamName, pPlayerName,pSport, pYear) {
     this.team = pTeamName;
     this.player = pPlayerName;
     this.sport=pSport;
+    this.year = pYear;
+    
+    
 }
-
-
-
 
 
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -18,13 +18,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
         let playername=document.getElementById("playername").value;
         let teamname=document.getElementById("teamname").value;
+        let year=document.getElementById("year").value;
 
         document.getElementById("default-text").style.display="none";
 
-        if (playername!=""&& teamname!=""&& selectedType!=""){
+        if (playername!=""&& teamname!=""&& year!=""&& selectedType!=""){
 
-            sportsArray.push ( new SportObject(playername,teamname,selectedType) );
-           
+            sportsArray.push ( new SportObject(teamname,playername,selectedType,year) );
 
         }
 
@@ -33,7 +33,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
             alert("Please enter all the details to save")
         }
         document.getElementById("playername").value="";
-        document.getElementById("teamname").value="";    
+        document.getElementById("teamname").value="";
+        document.getElementById("year").value = "";    
 
     });
 
@@ -41,20 +42,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
         selectedType = document.getElementById("select-type").value;
     });
 
-    $(document).on("pagebeforeshow", "#display", function (event) {   // have to use jQuery 
+    $(document).on("pagebeforeshow", "#display", function (event) {  
         createList();
     });
 
 });
 
 function createList() {
-    // clear prior data
-    var myul = document.getElementById("myul");
+    let myul = document.getElementById("myul");
     myul.innerHTML="";
-    sportsArray.forEach(function (element,) {   // use handy array forEach method
-        var li = document.createElement('li');
-        li.innerHTML = element.sport + ": Your Favourite team is - " + element.team +
-        ", and your favourite player is - " + element.player;
+    sportsArray.forEach(function (element,) {   
+        let li = document.createElement('li');
+        li.innerHTML = element.sport + ": Your Favourite team is - " + element.team + 
+        "<br> &emsp; &emsp; &emsp;Your favourite player is - " + element.player +" <br>&emsp; &emsp; &emsp; They were founded in "+ element.year;
         myul.appendChild(li);
     });
 };
