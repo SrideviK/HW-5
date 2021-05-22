9fb828ee36e6e9afcde18c859e40cdc40bcb8b81
SrideviK <sridevibasavaraju@gmail.com>
2021-05-21 18:37:51 -0700

    added third page

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let sportsArray = [];
 let selectedType = "";
+let subSetType="";
+let subList=[];
 
 // define a constructor to create sport objects
 let SportObject = function (pTeamName, pPlayerName,pSport, pYear) {
@@ -20,7 +22,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
         let teamname=document.getElementById("teamname").value;
         let year=document.getElementById("year").value;
 
-        document.getElementById("default-text").style.display="none";
+        document.getElementById("default-text1").style.display="none";
+        document.getElementById("default-text2").style.display="none";
+        document.getElementById("select-sport").style.display="flex";
 
         if (playername!=""&& teamname!=""&& year!=""&& selectedType!=""){
 
@@ -46,6 +50,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
         createList();
     });
 
+    $(document).on("pagebeforeshow", "#displaysubset", function (event) {  
+        document.getElementById("selected-list").style.display="none";
+    });
+
+    $(document).bind("change", "#subSetType", function (event, ui) {
+        subSetType = document.getElementById("subSetType").value;
+    });
+    document.getElementById("Show").addEventListener("click", function () {
+
+        createSubList(subSetType);
+
+    });
+
 });
 
 function createList() {
@@ -58,4 +75,36 @@ function createList() {
         myul.appendChild(li);
     });
 };
+
+function createSubList(sport){
+
+
+    let selected= document.getElementById("selected-list");
+    selected.style.display="block";
+    while (selected.firstChild) {    
+        selected.removeChild(selected.firstChild);
+    };
+    let ul = document.createElement('ul');
+    sportsArray.forEach(function (element,) { 
+
+        if(element.sport==sport){
+
+            let li = document.createElement('li');
+            li.innerHTML = element.sport + ": Your Favourite team is - " + element.team + 
+        "<br> &emsp; &emsp; &emsp;Your favourite player is - " + element.player +" <br>&emsp; &emsp; &emsp; They were founded in "+ element.year;
+        ul.appendChild(li);
+
+        }
+    
+    });
+    if(ul.childElementCount==0){
+        let p = document.createElement('p');
+        p.innerHTML="No details have been added for this sport";
+        selected.appendChild(p);
+    }
+    else{
+        selected.appendChild(ul);
+    }
+    
+}
 
